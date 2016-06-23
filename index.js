d72f01e30530fb5447453ee458b57c1fdb88acbb
Laurent Janet <laurent.janet@backelite.com>
2016-06-23 18:26:30 +0200

    use cors module

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+'use strict';
+
 let app = require('express')();
 let http = require('http').Server(app);
+let cors = require('cors');
+
+app.use(cors());
 
 http.listen(3000, () => {
     console.log('listenning on *: 3000');
