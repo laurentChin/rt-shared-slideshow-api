b4b835d23f0823e20d5162bd084c112d21a80da4
Laurent Janet <laurent.janet@backelite.com>
2016-06-23 22:29:36 +0200

    create uploads GET route

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ let http = require('http').Server(app);
 let cors = require('cors');
 let fileUpload = require('express-fileupload');
 const moment = require('moment');
+const fs = require('fs');
 
 app.use(cors());
 app.use(fileUpload());
 
-app.post('/upload', (request, response) => {
+app.post('/uploads', (request, response) => {
     if (!request.files) {
        response.status(400).send('no files uploaded');
     }
@@ -25,6 +26,13 @@ app.post('/upload', (request, response) => {
         console.log(path + ' moved in uploads');
         response.send('file uploaded');
     });
+});
+
+app.get('/uploads', (request, response) => {
+    let pictures = [];
+
+    pictures = fs.readdirSync('uploads');
+    response.send(pictures);
 });
 
 http.listen(3000, () => {
