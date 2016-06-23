2f5eebe46e03eadc13e9259554762768e4057e19
Laurent Janet <laurent.janet@backelite.com>
2016-06-23 20:25:35 +0200

    use express-fileupload

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let cors = require('cors');
+let fileUpload = require('express-fileupload');
 
 app.use(cors());
+app.use(fileUpload());
 
 http.listen(3000, () => {
     console.log('listenning on *: 3000');
