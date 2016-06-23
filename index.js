9fe8aaff0e5e7c9485cc87fa07f65afc7c38484a
Laurent Janet <laurent.janet@backelite.com>
2016-06-23 20:43:12 +0200

    implements file upload

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ let app = require('express')();
 let http = require('http').Server(app);
 let cors = require('cors');
 let fileUpload = require('express-fileupload');
+const moment = require('moment');
 
 app.use(cors());
 app.use(fileUpload());
+
+app.post('/upload', (request, response) => {
+    if (!request.files) {
+       response.status(400).send('no files uploaded');
+    }
+
+    let picture = request.files.picture;
+    let path = 'picture-' + moment().format('YYYYMMDD-HHmmss') + '-' + picture.name;
+
+    picture.mv('uploads/' + path, (err) => {
+        if (err) {
+           response.status(500).send(err);
+        }
+
+        console.log(path + ' moved in uploads');
+        response.send('file uploaded');
+    });
+});
 
 http.listen(3000, () => {
     console.log('listenning on *: 3000');
